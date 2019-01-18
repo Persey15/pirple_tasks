3279d6db63e4edac194d5235b00b606dac8df1be
Persey15 <33005224+Persey15@users.noreply.github.com>
2019-01-18 14:19:09 +0300

    Add files via upload

---
+++ song.js
@@ -0,0 +1,17 @@
+
+var genre = "Rock";
+var title= "Deep ocean";
+var releaseYear = 2014;
+var durationSeconds = 284;
+var album = "Clouds";
+var productionTourIsON = true;
+var artists = ["Dawid Bowi", "Michail Jackson", "Elton John"];
+
+console.log(genre);
+console.log(title);
+console.log(releaseYear);
+console.log(durationSeconds);
+console.log(album);
+console.log(productionTourIsON);
+console.log(artists);
+
